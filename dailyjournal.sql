f2d91d786109c3dc556a6aa358ce88e050ea146b
Dylan Morris <dylanrobertmorris@gmail.com>
2021-04-13 13:36:12 -0500

    made some database entries

--- dailyjournal.sql
+++ dailyjournal.sql
@@ -8,7 +8,15 @@ CREATE TABLE 'Entries' (
 );
 
 CREATE TABLE 'Moods' (
-    'id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+    'id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     'label' TEXT NOT NULL
 );
 
+INSERT INTO Moods VALUES (1, 'Fine');
+INSERT INTO Moods VALUES (2, 'Proud');
+INSERT INTO Moods VALUES (3, 'Frusturated');
+INSERT INTO Moods VALUES (4, 'Overwhelmed');
+
+INSERT INTO Entries VALUES (1, 'Event Listeners', 'They capture stuff from the dom and send info to other components', '2021-02-16', 2);
+INSERT INTO Entries VALUES (2, 'Debugger', "The debugger is useful but you have to know where to inset it. \n\nI just debugged code by inserting it in my MoodFilter.js to notice that my return statement wasn't returning code because the code was unreachable", '2021-02-16', 2);
+INSERT INTO Entries VALUES (3, 'Deleting', 'We learned the delete method in fetch calls to delete entries from a database by targeting their id', '2021-02-16', 1);
