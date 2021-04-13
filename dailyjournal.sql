1158a855f6bbb338ad525472f847c6ae66f30e05
Dylan Morris <dylanrobertmorris@gmail.com>
2021-04-13 13:23:14 -0500

    second commit

---
+++ dailyjournal.sql
@@ -0,0 +1,14 @@
+CREATE TABLE 'Entries' (
+    'id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    'concept' TEXT NOT NULL,
+    'entry' TEXT NOT NULL,
+    'date' DATE NOT NULL,
+    'mood_id' INTEGER NOT NULL,
+    FOREIGN KEY('mood_id') REFERENCES 'Mood'('id')
+);
+
+CREATE TABLE 'Moods' (
+    'id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+    'label' TEXT NOT NULL
+);
+
