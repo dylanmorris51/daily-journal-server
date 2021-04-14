e1bb05dfff3c9575128dbb694b61552fa7f0d507
Dylan Morris <dylanrobertmorris@gmail.com>
2021-04-14 15:14:00 -0500

    both functions working in sql

--- dailyjournal.sql
+++ dailyjournal.sql
@@ -20,3 +20,13 @@ INSERT INTO Moods VALUES (4, 'Overwhelmed');
 INSERT INTO Entries VALUES (1, 'Event Listeners', 'They capture stuff from the dom and send info to other components', '2021-02-16', 2);
 INSERT INTO Entries VALUES (2, 'Debugger', "The debugger is useful but you have to know where to inset it. \n\nI just debugged code by inserting it in my MoodFilter.js to notice that my return statement wasn't returning code because the code was unreachable", '2021-02-16', 2);
 INSERT INTO Entries VALUES (3, 'Deleting', 'We learned the delete method in fetch calls to delete entries from a database by targeting their id', '2021-02-16', 1);
+
+
+SELECT
+    e.id,
+    e.concept,
+    e.entry,
+    e.mood_id,
+    e.date
+FROM entries e
+WHERE e.id = 2
