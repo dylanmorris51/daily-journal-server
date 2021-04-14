fab905c1f0a01807c179fd16dece3294e632716c
Dylan Morris <dylanrobertmorris@gmail.com>
2021-04-14 15:12:30 -0500

    got single entry fetch call working postman

--- entries/request.py
+++ entries/request.py
@@ -27,3 +27,25 @@ def get_all_entries():
             entries.append(entry.__dict__)
 
         return json.dumps(entries)
+
+def get_single_entry(id):
+    with sqlite3.connect("./dailyjournal.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+            SELECT
+                e.id,
+                e.concept,
+                e.entry,
+                e.mood_id,
+                e.date
+            FROM entries e
+            WHERE e.id = ?
+        """, (id,))
+
+        data = db_cursor.fetchone()
+
+        entry = Entry(data['id'], data['concept'], data['entry'], data['mood_id'], data['date'])
+
+        return json.dumps(entry.__dict__)
